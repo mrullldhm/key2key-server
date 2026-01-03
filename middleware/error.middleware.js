6a7b6247f21686dad05044eab39dd87ba02941dc
Muhamad Amirul Adham <154552382+mrullldhm@users.noreply.github.com>
2026-01-04 03:18:55 +0800

    Add prisa errorhandler and jwt expired errorhandler

--- middleware/error.middleware.js
+++ middleware/error.middleware.js
@@ -3,7 +3,21 @@ const errorMiddleware = (err, req, res, next) => {
   try {
     let error = { ...err };
     error.message = err.message;
+
+    // Log for the developer
     console.error(err);
+
+    // Prisma Unique Constraint Error (e.g., Email already exists)
+    if (err.code === "P2002") {
+      error.message = "The information provided is already in use.";
+      error.statusCode = 409;
+    }
+
+    // JWT Expired Error
+    if (err.name === "TokenExpiredError") {
+      error.message = "Your session has expired. Please log in again.";
+      error.statusCode = 401;
+    }
 
     res
       .status(error.statusCode || 500)
@@ -14,4 +28,3 @@ const errorMiddleware = (err, req, res, next) => {
 };
 
 export default errorMiddleware;
-
